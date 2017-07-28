9a474c403ed76490e7d9ad41b8a3d5c30fc48971
zhaogang <zgyxx091418@163.com>
2017-07-28 09:16:37 +0800

    导控接口对接完成。。。。。

--- src/main/java/com/syx/pilotcontrol/module/guidance/service/imp/TaskService.java
+++ src/main/java/com/syx/pilotcontrol/module/guidance/service/imp/TaskService.java
@@ -20,6 +20,8 @@ import java.util.Random;
  */
 @Service
 public class TaskService implements ITaskService {
+    private final String pilcontrolUrl = "http://10.0.0.36:18080/api/guide";
+
     @Autowired
     BaseDao baseDao;
 
@@ -31,6 +33,7 @@ public class TaskService implements ITaskService {
         String[] numberTypes = (jsonObject.getString("number_type")).split(",");
         String taskType = jsonObject.getString("task_type");
         String taskUrl = jsonObject.getString("task_url");
+        String taskTitle = jsonObject.getString("task_title");
         String taskNumber = jsonObject.getString("task_number");
         String intervalTime = jsonObject.getString("interval_time");
         String sqlGetNumber = "";
@@ -69,30 +72,19 @@ public class TaskService implements ITaskService {
         }
         // 所有的系统的账号
         int jsonArrayNumberLen = jsonArrayNumber.size();
-
-        if (taskType.equals("1")) {
-            // 发帖，只有标题和内容
-            for (int i = 0; i < Integer.parseInt(taskNumber, 10); i++) {
-                int randomNumber = (int) (Math.random() * jsonArrayNumberLen);
-                /*Map<String, String> mapPost = new HashMap<>();
-                HttpClientUtil.sendPost("", mapPost);*/
-            }
-        }
-        if (taskType.equals("2")) {
-            // 回帖，只有标题和内容
-            for (int i = 0; i < Integer.parseInt(taskNumber, 10); i++) {
-                int randomNumber = (int) (Math.random() * jsonArrayNumberLen);
-                int randomContext = (int) (Math.random() * taskContextsLen);
-                JSONObject jsonObjectNumber = jsonArrayNumber.getJSONObject(randomNumber);
-                Map<String, String> mapPost = new HashMap<>();
-                mapPost.put("type", taskType);
-                mapPost.put("account", /*jsonObjectNumber.getString("number_name")*/"lkwlkw22");
-                mapPost.put("password", /*jsonObjectNumber.getString("number_password")*/"lkwlkw222");
-                mapPost.put("url", "http://bbs.212300.com/thread-4335178-1-1.html");
-                mapPost.put("title", " 1");
-                mapPost.put("content", taskContexts[randomContext]);
-                /*HttpClientUtil.sendPost("http://10.0.0.36:18080/api/guide", mapPost);*/
-            }
+        // type 1代表发帖（需要标题和内容），2代表回帖 （只需要语料的内容）
+        for (int i = 0; i < Integer.parseInt(taskNumber, 10); i++) {
+            int randomNumber = (int) (Math.random() * jsonArrayNumberLen);
+            int randomContext = (int) (Math.random() * taskContextsLen);
+            JSONObject jsonObjectNumber = jsonArrayNumber.getJSONObject(randomNumber);
+            Map<String, String> mapPost = new HashMap<>();
+            mapPost.put("type", taskType);
+            mapPost.put("account", jsonObjectNumber.getString("number_name"));
+            mapPost.put("password", jsonObjectNumber.getString("number_password"));
+            mapPost.put("url", taskUrl);
+            mapPost.put("title", taskTitle);
+            mapPost.put("content", taskContexts[randomContext]);
+            HttpClientUtil.sendPost(pilcontrolUrl, mapPost);
         }
         jsonObject.put("result", result);
         return jsonObject;
