2752288baa57f1266bcf4588815e6895f707946f
zhaogang <zgyxx091418@163.com>
2017-08-28 18:07:31 +0800

    jwt实现接口拦截

--- src/main/java/com/syx/pilotcontrol/module/manpower/service/IOtherTaskService.java
+++ src/main/java/com/syx/pilotcontrol/module/manpower/service/IOtherTaskService.java
@@ -3,5 +3,5 @@ package com.syx.pilotcontrol.module.manpower.service;
 /**
  * Created by Msater Zg on 2017/8/28.
  */
-public class IOtherTaskService {
+public interface IOtherTaskService {
 }
--- src/main/java/com/syx/pilotcontrol/module/manpower/service/imp/OtherTaskService.java
+++ src/main/java/com/syx/pilotcontrol/module/manpower/service/imp/OtherTaskService.java
@@ -1,7 +1,9 @@
 package com.syx.pilotcontrol.module.manpower.service.imp;
 
+import com.syx.pilotcontrol.module.manpower.service.IOtherTaskService;
+
 /**
  * Created by Msater Zg on 2017/8/28.
  */
-public class OtherTaskService {
+public class OtherTaskService implements IOtherTaskService {
 }
